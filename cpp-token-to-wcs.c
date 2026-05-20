0bc67509911e5d7fe50924287e9db24fc92c9b86
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2026-05-19 18:59:26 -0700

    correct cpp-token-to-wcs.c
    Replace printing of length with a to-multibyte conversion and printing of the
    wide string, correct which headers are included.
    
    In testing, I'd forgotten that the GCC -std=c89 flag doesn't stop you from
    including newer headers. C89 had wide string literals and wchar_t, of course,
    but did not have <wchar.h>.

--- cpp-token-to-wcs.c
+++ cpp-token-to-wcs.c
@@ -23,27 +23,26 @@
  * void my_prefix_putc(char c);
  *
  * It follows, therefore, that we can create a macro that converts an identifier
- * to a wide string literal with L###some_id. Requires C89 or later. */
+ * to a wide string literal with L###some_id. Requires C89 or later.
+ *
+ * (You'll probably want C95 or later if you actually intend to do anything
+ * serious with wide strings, though.) */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <wchar.h>
+#include <stddef.h>
 
 #define WCSTR(id) L###id
 
 int main(void) {
     wchar_t* ws = WCSTR(foobar);
-    printf("%"
-           #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
-               "z"
-           #else
-               "l"
-           #endif
-           "d\n",
-           #if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
-               (unsigned long int)
-           #endif
-           wcslen(ws));
+
+    {
+        char buf[4096];
+        if (wcstombs(buf, ws, 4096) == (size_t)(-1)) return EXIT_FAILURE;
+        fputs(buf, stdout);
+    }
+
     return EXIT_SUCCESS;
 }
